aa6df5aa23246b17e6a061a2f8220c77fdbb3ce4
littletree <602788458@qq.com>
2018-11-27 07:43:04 +0800

    11.27

--- app/src/main/java/com/example/littletreestronger/MainActivity.kt
+++ app/src/main/java/com/example/littletreestronger/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.littletreestronger
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,22 +11,8 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
+
     }
 
-
-
-
-//    /**
-//     * A native method that is implemented by the 'native-lib' native library,
-//     * which is packaged with this application.
-//     */
-//    external fun stringFromJNI(): String
-//
-//    companion object {
-//
-//        // Used to load the 'native-lib' library on application startup.
-//        init {
-//            System.loadLibrary("native-lib")
-//        }
-//    }
 }
