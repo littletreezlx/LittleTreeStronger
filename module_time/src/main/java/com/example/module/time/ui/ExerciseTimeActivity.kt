628ee9a7c92bca6c41f7b2f376b558c49f3a9d05
vincent <602788458@qq.com>
2019-01-25 17:18:18 +0800

    rxpermission2

--- module_time/src/main/java/com/example/module/time/ui/ExerciseTimeActivity.kt
+++ module_time/src/main/java/com/example/module/time/ui/ExerciseTimeActivity.kt
@@ -1,6 +1,11 @@
 package com.example.module.time.ui
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.KeyEvent
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.alibaba.android.arouter.facade.annotation.Route
@@ -10,6 +15,7 @@ import com.example.module.time.data.ExerciseRecord
 import com.example.module.time.di.exerciseTimeDiModule
 import com.example.module.time.viewmodel.ExerciseTimeViewModel
 import com.google.gson.Gson
+import com.tbruyelle.rxpermissions2.RxPermissions
 import org.jetbrains.anko.*
 import org.jetbrains.anko.recyclerview.v7.recyclerView
 import org.jetbrains.anko.sdk27.coroutines.onClick
@@ -25,6 +31,19 @@ import org.kodein.di.generic.instance
 @Route(path = "/time/exercise")
 class ExerciseTimeActivity: BaseActivity(), KodeinAware{
 
+
+//    <uses-permission android:name="android.permission.BLUETOOTH" />
+//    <uses-permission android:name="android.permission.INTERNET" />
+//    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+//    <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />
+//    <!-- Required to check if WiFi is enabled -->
+//    <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
+//    <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION" />
+//    <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
+//    <uses-permission android:name="android.permission.READ_PHONE_STATE" />
+//    <uses-permission android:name="android.permission.CHANGE_WIFI_STATE" />
+//    <uses-permission android:name="android.permission.ACCESS_WIFI_STATE" />
+//    <uses-permission android:name="android.permission.WAKE_LOCK" />
 
     private val parentKodein by closestKodein()
 
@@ -116,6 +135,99 @@ class ExerciseTimeActivity: BaseActivity(), KodeinAware{
             recordAdapter.notifyDataSetChanged()
         })
 
+
+
+        requestPermissions()
+
 //        val viewModel = ViewModelProviders.of(this).get(ExerciseTimeViewModel::class.java)
     }
+
+
+    fun requestPermissions(){
+
+        RxPermissions(this)
+            .requestEach(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_PHONE_STATE
+            )
+            .subscribe { permission ->
+                if (permission.granted) {
+                    //agree
+//                    onGranted()
+                } else {
+                    showDialog(permission.name)
+                }
+            }
+
+
+
+//            .subscribe(new Consumer<Permission>() {
+//                @Override
+//                public void accept(Permission permission) throws Exception {
+//                    if (permission.granted) {
+//                        // 用户已经同意该权限
+//                        Log.d(TAG, permission.name + " is granted.");
+//                    } else if (permission.shouldShowRequestPermissionRationale) {
+//                        // 用户拒绝了该权限，没有选中『不再询问』（Never ask again）,那么下次再次启动时，还会提示请求权限的对话框
+//                        Log.d(TAG, permission.name + " is denied. More info should be provided.");
+//                    } else {
+//                        // 用户拒绝了该权限，并且选中『不再询问』
+//                        Log.d(TAG, permission.name + " is denied.");
+//                    }
+//                }
+//            });
+
+    }
+
+
+
+    fun showDialog(permissionName : String){
+        val keylistener = DialogInterface.OnKeyListener { _, keyCode, event ->
+            keyCode == KeyEvent.KEYCODE_BACK && event.repeatCount == 0
+        }
+
+        var dialog = AlertDialog.Builder(this)
+            .setTitle("权限申请")
+            .setMessage("${permissionName}为必选项，开通后方可正常使用APP,请在设置中开启。")
+            .setOnKeyListener(keylistener)
+            .setCancelable(false)
+            .setPositiveButton("去开启") { _, _ ->
+                //                                JumpPermissionManagement.GoToSetting(this)
+
+                finish()
+            }
+            .setNegativeButton("结束") { _, _ ->
+                Toast.makeText(this, "${permissionName}权限未开启，不能使用该功能！", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            .create()
+
+        dialog.show()
+    }
+
+
+    //传入权限与权限描述，在需要权限的功能打开之前调用
+    fun requestPermissions(vararg permissions: String, describe: String, onGranted:()->Unit) {
+//        val keylistener = DialogInterface.OnKeyListener { _, keyCode, event ->
+//            keyCode == KeyEvent.KEYCODE_BACK && event.repeatCount == 0
+//        }
+//
+//        var dialog = AlertDialog.Builder(this)
+//            .setTitle("权限申请")
+//            .setMessage("${describe}为必选项，开通后方可正常使用APP,请在设置中开启。")
+//            .setOnKeyListener(keylistener)
+//            .setCancelable(false)
+//            .setPositiveButton("去开启") { _, _ ->
+//                //                JumpPermissionManagement.GoToSetting(this)
+//                finish()
+//            }
+//            .setNegativeButton("结束") { _, _ ->
+//                Toast.makeText(this, "${describe}权限未开启，不能使用该功能！", Toast.LENGTH_SHORT).show()
+//                finish()
+//            }
+//            .create()
+    }
+
+
 }
